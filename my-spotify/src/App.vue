265182f70e0657ddf261e9a66c598d1a3d20c1ef
chauanphu <chauanphu@gmail.com>
2021-11-21 15:31:44 +0700

    add spotify logo

--- my-spotify/src/App.vue
+++ my-spotify/src/App.vue
@@ -1,17 +1,19 @@
 <template>
   <div class="bg-dark h-screen">
-    <div class="flex h-4/5">
+    <div class="flex h-5/6">
       <!-- Side Nav -->
-      <aside class="bg-black w-56 h-full flex-none"></aside>
+      <aside class="bg-black w-56 h-full flex-none">
+        <div>
+          <img src="./assets/Spotify_Logo_RGB_White.png" alt="Spotify Logo" class="h-10" />
+        </div>
+      </aside>
       <!-- Main Content -->
       <main class="w-full h-full bg-blue-400 relative">
         <!-- Header -->
         <div class="w-full sticky top-0 bg-yellow-300 p-2"></div>
       </main>
     </div>
-    <div class="w-full bg-light h-1/5">
-  
-    </div>
+    <div class="w-full bg-light h-1/6"></div>
   </div>
 </template>
 
