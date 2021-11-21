e9efd99f34343eac1f0b5fa1b2f913376bb1ea07
chauanphu <chauanphu@gmail.com>
2021-11-21 15:21:01 +0700

    create layout

--- my-spotify/src/App.vue
+++ my-spotify/src/App.vue
@@ -1,7 +1,19 @@
 <template>
   <div class="bg-dark h-screen">
-    <h1 class="text-center">Hi</h1>
+    <div class="flex h-4/5">
+      <!-- Side Nav -->
+      <aside class="bg-black w-56 h-full flex-none"></aside>
+      <!-- Main Content -->
+      <main class="w-full h-full bg-blue-400 relative">
+        <!-- Header -->
+        <div class="w-full sticky top-0 bg-yellow-300 p-2"></div>
+      </main>
+    </div>
+    <div class="w-full bg-light h-1/5">
+  
+    </div>
   </div>
 </template>
 
-<style></style>
+<style scoped>
+</style>
